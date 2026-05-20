f26a60886d17afbdc7bfa72196f0095eb0ba286b
sohei <hewitt.so@mail.utoronto.ca>
2026-05-20 17:07:46 -0400

    Entries added will be displayed below the textfield, might be changing the HistoryPane to a TableView cause that looks like a better solution

--- src/view/HistoryPane.java
+++ src/view/HistoryPane.java
@@ -5,38 +5,49 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import model.BudgetModel;
 
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
 public class HistoryPane extends GridPane implements Observer {
-    //private final BudgetModel model;
-    private Text curritem;
-    private Text curramount;
+    private final BudgetModel model;
+    private int count = 1;
+    //private Text curritem;
+    //private Text curramount;
+    private ArrayList<String> itemlist = new ArrayList<>();
+    private ArrayList<Integer> amountlist = new ArrayList<>();
     public HistoryPane(BudgetModel model) {
-        //this.model = model;
-        curritem = new Text("");
-        curramount = new Text("slides");
+        this.model = model;
+        //Text curritem = new Text("curri");
+        //Text curramount = new Text("curra");
+        this.setMinSize(600, 600);
         this.setAlignment(Pos.TOP_CENTER);
 
-        this.add(curritem, 0, 0);
-        this.add(curramount, 1, 0);
+        //this.add(curritem, 0, 0);
+        //this.add(curramount, 1, 0);
     }
+
     /**
-     * This method is called whenever the observed object is changed. An
-     * application calls an {@code Observable} object's
-     * {@code notifyObservers} method to have all the object's
-     * observers notified of the change.
-     *
-     * @param o   the observable object.
-     * @param arg an argument passed to the {@code notifyObservers}
-     *            method.
+     * Add item and amount values into corresponding arraylist
+     * Can track order via index number?
+     * @param citem
+     * @param camount
      */
+    private void addEntry(String citem, int camount){
+        itemlist.add(citem);
+        amountlist.add(camount);
+        Text item = new Text(citem);
+        Text amount = new Text(camount + "");
+        this.add(item, 0, this.count);
+        this.add(amount, 1, this.count);
+        this.count += 1;
+    }
+
+
     @Override
     public void update(Observable o, Object arg) {
-        BudgetModel model = (BudgetModel) o;
-        String item = model.getcurritem();
-        int amount = model.getcurramount();
-        curritem.setText(item);
-        curramount.setText(Integer.toString(amount));
+        String ci = model.getcurritem();
+        int ca = model.getcurramount();
+        this.addEntry(ci, ca);
     }
 }
